ef845f983ce713b56f5f189faa5a45fd14ef86ac
siddhinarayan09 <siddhinarayan.nitrkl@gmail.com>
2024-12-17 23:14:16 +0530

    modified:   app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify, redirect, render_template_string
 import joblib
 import json
+import os
 
 app = Flask(__name__)
 
@@ -69,3 +70,6 @@ def chatbot_response():
     
 if __name__ == "__main__":
     app.run(debug=True)
+# Get the port from the environment variable, default to 5000 if not set
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
